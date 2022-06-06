0948cc68790ddedff0194dfb279ba2efdfde9e83
Caua Veiga <cauaveiga@gmail.com>
2022-06-06 20:51:50 +0100

    Identificar usúrios

--- main.py
+++ main.py
@@ -4,19 +4,12 @@
 from mfrc522 import MFRC522
 from machine import Pin, SPI
 
+ids = {'0x94006b1a': ['Chaveiro', 'Cauã Veiga'],
+       '0x9351e2ec': ['Cartão','Rafael Lobão']}
 
 spi = SPI(2, baudrate=5000000, polarity=0, phase=0,
           sck=Pin(18), mosi=Pin(23), miso=Pin(19))
-# Using Hardware SPI pins:
-#     sck=18   # yellow
-#     mosi=23  # orange
-#     miso=19  # blue
-#     rst=4    # white
-#     cs=5     # green, DS
-# *************************
-# To use SoftSPI,
-# from machine import SOftSPI
-# spi = SoftSPI(baudrate=100000, polarity=0, phase=0, sck=sck, mosi=mosi, miso=miso)
+
 spi.init()
 rdr = MFRC522(spi=spi, gpioRst=25, gpioCs=5)
 print("Place card")
@@ -28,8 +21,9 @@ try:
         if stat == rdr.OK:
             (stat, raw_uid) = rdr.anticoll()
             if stat == rdr.OK:
-                card_id = "uid: 0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
-                print(card_id)
+                card_id = "0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
+                print(f'{ids[card_id][0]}: {ids[card_id][1]}')
+                print('')
 except KeyboardInterrupt:
     print('Keyboard Interrupt')
 
