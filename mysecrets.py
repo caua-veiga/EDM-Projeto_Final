22eea7348eab1bdade04af38b75adbb9a17b5999
raclobao <62575737+raclobao@users.noreply.github.com>
2022-06-25 20:59:54 +0100

    api_key aqui definida!

--- mysecrets.py
+++ mysecrets.py
@@ -1,3 +1,3 @@
 ssid = 'mySSID'
 password = 'myPassword'
-api_key = 'lnYYvp6TN0pSI_E47x641Fwe9JynxSD2glVE-OKLysI' #Não alterar!
+api_key = 'lnYYvp6TN0pSI_E47x641Fwe9JynxSD2glVE-OKLysI'  #Não alterar!
