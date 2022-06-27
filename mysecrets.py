3eb0ff985e5767039f99a0d64f46f3bdd638b436
Caua Veiga <cauaveiga@gmail.com>
2022-06-27 14:59:55 +0100

    Reorganizar Pastas

--- mysecrets.py
+++
@@ -1,7 +0,0 @@
-enderecos = {
-    'casa_caua': ('MEO-26F450', 'e2f77e6a0d'),
-    'iphone_caua': ('iPhone Caua', 'caualex12')
-}
-
-ssid, password = enderecos['iphone_caua'] 
-api_key = 'lnYYvp6TN0pSI_E47x641Fwe9JynxSD2glVE-OKLysI'  #Não alterar!
