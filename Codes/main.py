78bc55714b18aa11797aa68af807584d1c86b587
Caua Veiga <cauaveiga@gmail.com>
2022-06-29 19:42:40 +0100

    Registro tempo atual e guarda cartões novos em .json, assim estarão sempre registrados

--- Codes/main.py
+++ Codes/main.py
@@ -6,14 +6,23 @@ from machine import Pin, SPI
 import socket
 import time
 import uasyncio
+import urequests
+import json
 
 
-# Defining Led Pins
+# Função para pegar o tempo local
+async def get_time():
+    link = 'https://timeapi.io/api/Time/current/zone?timeZone=Europe/Lisbon'
+    x = json.loads(urequests.get(link).text)
+    return f"{x['year']}/{x['month']}/{x['day']}  {x['hour']}:{x['minute']}:{x['seconds']}"
+
+
+# Definindo Led Pins
 led_red = Pin(12, Pin.OUT)
 led_green = Pin(15, Pin.OUT)
 
 
-# Setting all leds to False
+# Colocando os Leds para falso (desligado)
 led_red.value(False)
 led_green.value(False)
 
@@ -26,10 +35,10 @@ async def blink(output):
         led_off = led_red
         led_on = led_green
 
-    led_off.off()
-    led_on.on()
+    led_off.value(False)
+    led_on.value(True)
     await uasyncio.sleep_ms(1200)
-    led_on.off()
+    led_on.value(False)
 
 
 spi = SPI(2, baudrate=5000000, polarity=0, phase=0,
@@ -37,14 +46,22 @@ spi = SPI(2, baudrate=5000000, polarity=0, phase=0,
 
 spi.init()
 rdr = MFRC522(spi=spi, gpioRst=25, gpioCs=5)
+
+with open('registros.json') as json_file:
+        ids = json.load(json_file)
+
 async def main():
+    global ids
+
+    link = 'https://timeapi.io/api/Time/current/zone?timeZone=Europe/Lisbon'
+
 
     ENTRADA_SAIDA = 2
 
     # IDS: 'ID COMUNICAR COM OBJETO' : [OBJETO (CHAVEIRO OU CARTAO), NOME USUARIO (QLQR, APENAS INTERFACE), ESTADO (LIBERAR OU NN)
     # -- , CONTROLO ENTRADA OU SAIDA (0 OU 1)]
-    ids = {'0x94006b1a': ['Chaveiro', 'Cauã Veiga',0],
-        '0x9351e2ec': ['Cartão','Felipe Coutinho',-1]} # TROCAR ISSO PARA UM JSON
+    #ids = {'0x94006b1a': ['Chaveiro', 'Cauã Veiga',0],
+    #    '0x9351e2ec': ['Cartão','Rafael Lobão',-1]} # TROCAR ISSO PARA UM JSON
 
     #spi = SPI(2, baudrate=5000000, polarity=0, phase=0,
     #        sck=Pin(18), mosi=Pin(23), miso=Pin(19))
@@ -103,7 +120,7 @@ async def main():
                         print('O cartão ainda não está registrado, por favor entre as informações a seguir: ')
                         ob = input('Objeto: ')
                         nm = input('Nome: ')
-                        es = input('Estado (-1:Negado||0:Liberado): ') # -1= negado, 0=liberado
+                        es = int(input('Estado (-1:Negado||0:Liberado): ')) # -1= negado, 0=liberado
                         ids[card_id] = [ob,nm,es]
 
                     print(f'{ids[card_id][0]}: {ids[card_id][1]} --',end=' ')
@@ -111,12 +128,12 @@ async def main():
                     #Para o caso em que é liberado
                     output = 'Negado'
                     # Se estiver a entrar
-                    if ids[card_id][ENTRADA_SAIDA] == 0:
+                    if int(ids[card_id][ENTRADA_SAIDA]) == 0:
                         #print(f'Entrada Liberada')
                         output = 'Entrada Liberada'
                         ids[card_id][ENTRADA_SAIDA] = 1
                     # Se estiver a sair
-                    elif ids[card_id][ENTRADA_SAIDA] == 1:
+                    elif int(ids[card_id][ENTRADA_SAIDA]) == 1:
                         #print(f'Saída Liberada')
                         output = 'Saída Liberada'
                         ids[card_id][ENTRADA_SAIDA] = 0
@@ -129,7 +146,10 @@ async def main():
                     print(output)
                     print('')
 
-                    hist.append('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>' % ('hora', ids[card_id][0], ids[card_id][1], output))
+                    x = json.loads(urequests.get(link).text)
+                    time_ = f"{x['day']}/{x['month']}/{x['year']}  {x['hour']}:{x['minute']}:{x['seconds']}"
+                    #time_ = '------------'
+                    hist.append('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>' % (time_, ids[card_id][0], ids[card_id][1], output))
 
                     #print('antes cl')
                     cl, addr = s.accept()
@@ -157,3 +177,7 @@ except KeyboardInterrupt:
 
 finally:
     spi.deinit()
+
+    # Directly from dictionary
+    with open('registros.json', 'w') as outfile:
+        json.dump(ids, outfile)
